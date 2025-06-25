3d98961a64e2c76e13f1f3db59e4d04dd53c5c36
achak <136048425+Achak0120@users.noreply.github.com>
2025-06-24 19:05:08 -0500

    translator

--- backend/document_translator/translator.py
+++ backend/document_translator/translator.py
@@ -1,2 +1,10 @@
 from PIL import Image, ImageDraw, ImageFont
+import cv2
+import pytesseract
+import requests
+import os
 
+pytesseract.pytesseract.tesseract_cmd=r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+def image_to_string(image_path):
+    return pytesseract.image_to_string(Image.open(image_path))
