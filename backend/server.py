bb46d2d05a30232dc7e835e63e8073fe32f56d72
achak <136048425+Achak0120@users.noreply.github.com>
2025-06-26 18:22:18 -0500

    Backend

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask import request, send_file
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
+from document_translator import image_utils
+from document_translator import translator
+import os
 
 alibi_entry = Blueprint('Alibi Entry Point', __name__,
                         template_folder='templates')
@@ -13,3 +17,12 @@ def show(page):
         render_template(f'pages/{page}.html')
     except TemplateNotFound:
         abort(404)
+
+
+app = Flask(__name__)
+app.register_blueprint(alibi_entry)
+@app.route('/upload-image', methods=['POST'])
+def upload_image():
+    request.files['image']
+    request.form['targetLanguage']
+    
