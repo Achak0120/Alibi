5fe341207b03f52a18d4786fe740d5dcad8d65fc
achak <136048425+Achak0120@users.noreply.github.com>
2025-06-29 22:45:05 -0500

    overlay translated text function

--- backend/document_translator/image_utils.py
+++ backend/document_translator/image_utils.py
@@ -10,3 +10,9 @@ pytesseract.pytesseract.tesseract_cmd=r"C:\Program Files\Tesseract-OCR\tesseract
 # Outputs plain english text
 def image_to_string(image_path):
     return pytesseract.image_to_string(Image.open(image_path))
+
+def overlay_translated_text_on_image(original_image_path: str, translated_text: str, output_path: str):
+    image = Image.open(original_image_path)
+    data = pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT)
+    for i in range(len(data["text"])):
+        
--- backend/server.py
+++ backend/server.py
@@ -3,7 +3,7 @@ from flask import request, send_file
 from flask import Blueprint, render_template, abort, flash, redirect, url_for
 from jinja2 import TemplateNotFound
 from document_translator import image_utils
-from document_translator import translate
+from document_translator import translator
 import os
 
 alibi_entry = Blueprint('Alibi Entry Point', __name__,
@@ -36,4 +36,4 @@ def upload_image():
     
     extracted_text = image_utils.image_to_string(save_path)
     lang = request.form['targetLanguage']
-    translator.translate()
+    translated_text = translator.translate(extracted_text, lang)
